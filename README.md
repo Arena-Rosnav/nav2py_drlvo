ee5e56c088c5589294976d8d4831b90619410f1b
zzuxzt <zhanteng.xie@temple.edu>
2025-05-16 18:54:49 +0800

    First commit

--- README.md
+++ README.md
@@ -1,25 +1,59 @@
 # nav2py_drl_vo
 
-This repository integrates the ROS 2 Navigation Stack (Nav2) with a Deep Reinforcement Learning-based Velocity Obstacle (DRL-VO) control policy, enabling autonomous navigation through environments with static and dynamic obstacles.
+This repository provides the Nav2 implementation of DRL-VO navigation control policy, where the paper is ["DRL-VO: Learning to Navigate Through Crowded Dynamic Scenes Using Velocity Obstacles"](
+https://doi.org/10.1109/TRO.2023.3257549
+)([arXiv](https://arxiv.org/pdf/2301.06512.pdf)).
+Video demos can be found at [multimedia demonstrations](https://www.youtube.com/watch?v=KneELRT8GzU&list=PLouWbAcP4zIvPgaARrV223lf2eiSR-eSS&index=2&ab_channel=PhilipDames). The original training and implementation code can be found in our [drl_vo_nav](https://github.com/TempleRAIL/drl_vo_nav.git) repository. 
 
-## Installation
 
-1. **Install nav2py and deps**
+## Requirements:
+* Ubuntu 22.04
+* ROS2-Humble
+* Python 3.10.12
+* venv
 
-   ```bash
-      git clone https://github.com/voshch/nav2py
-      git clone https://github.com/voshch/ament_cmake_venv
-      git clone https://github.com/voshch/ament_cmake_venv_uv
-   ```
-   
-2. **Clone the Repository**:
+## 📌 Installation
 
-   ```bash
-   git clone https://github.com/Arena-Rosnav/nav2py_drlvo
+### **1. Install ROS2 (Humble) and Navigation 2**
+- [ROS2 Installation Guide](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html)
+- [Nav2 Installation Guide](https://docs.nav2.org/getting_started/index.html)
 
-3. Build workspace.
+### **2. Clone this repository into your workspace**
+```bash
+mkdir -p ~/nav2_ws/src
+cd ~/nav2_ws/src
+git clone -b humble https://github.com/TempleRAIL/drl_vo_nav.git
+```
 
-4. **Run** (in this directory):
-   ```bash
-   ros2 launch nav2_bringup tb4_simulation_launch.py params_file:=nav2_params.yaml headless:=True
-   ```
+### **3. Build the workspace**
+```bash
+cd ~/nav2_ws
+source /opt/ros/humble/setup.bash
+colcon build
+```
+
+### **4. Source the workspace**
+```bash
+source ~/nav2_ws/install/setup.bash
+```
+
+### **5. Run the Nav2 demo launch**
+```bash
+export TURTLEBOT3_MODEL=waffle
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_gazebo/models
+ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False params_file:=$(pwd)/src/nav2py_drl_vo/tb3_drl_vo_nav2_params.yaml
+```
+
+## Citation
+```
+@article{xie2023drl,
+  author={Xie, Zhanteng and Dames, Philip},
+  journal={IEEE Transactions on Robotics}, 
+  title={DRL-VO: Learning to Navigate Through Crowded Dynamic Scenes Using Velocity Obstacles}, 
+  year={2023},
+  volume={39},
+  number={4},
+  pages={2700-2719},
+  doi={10.1109/TRO.2023.3257549}
+}
+```
--- nav2py_drl_vo_controller/include/nav2py_drl_vo_controller/drl_vo_controller.hpp
+++ nav2py_drl_vo_controller/include/nav2py_drl_vo_controller/drl_vo_controller.hpp
@@ -4,70 +4,114 @@
  * Template Controller for nav2py
  */
 
- #ifndef NAV2PY_DRL_VO_CONTROLLER_HPP_
- #define NAV2PY_DRL_VO_CONTROLLER_HPP_
- 
- #include <string>
- #include <vector>
- #include <memory>
- 
- #include "nav2py/controller.hpp"
- #include "nav2py/utils.hpp"
- #include "rclcpp/rclcpp.hpp"
- #include "pluginlib/class_loader.hpp"
- #include "pluginlib/class_list_macros.hpp"
+#ifndef NAV2PY_TEMPLATE_CONTROLLER__TEMPLATE_CONTROLLER_HPP_
+#define NAV2PY_TEMPLATE_CONTROLLER__TEMPLATE_CONTROLLER_HPP_
 
- #include "sensor_msgs/msg/laser_scan.hpp"
+#include <string>
+#include <vector>
+#include <memory>
 
+#include "nav2py/controller.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
  
- namespace nav2py_drl_vo_controller
- {
- 
- class DRL_VO_Controller : public nav2py::Controller
- {
- public:
-   DRL_VO_Controller() = default;
-   ~DRL_VO_Controller() override = default;
- 
-   void configure(
-     const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
-     std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
-     const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
- 
-   void cleanup() override;
-   void activate() override;
-   void deactivate() override;
-   void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
- 
-   geometry_msgs::msg::TwistStamped computeVelocityCommands(
-     const geometry_msgs::msg::PoseStamped & pose,
-     const geometry_msgs::msg::Twist & velocity,
-     nav2_core::GoalChecker * goal_checker) override;
- 
-   void setPlan(const nav_msgs::msg::Path & path) override;
+namespace nav2py_drl_vo_controller
+{
+
+class DrlVoController : public nav2py::Controller
+{
+public:
+    DrlVoController() = default;
+    ~DrlVoController() override = default;
+  
+    void configure(
+      const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+      std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
+      const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+  
+    void cleanup() override;
+    void activate() override;
+    void deactivate() override;
+    void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
+  
+    geometry_msgs::msg::TwistStamped computeVelocityCommands(
+      const geometry_msgs::msg::PoseStamped & pose,
+      const geometry_msgs::msg::Twist & velocity,
+      nav2_core::GoalChecker * goal_checker) override;
+  
+    void setPlan(const nav_msgs::msg::Path & path) override;
  
  protected:
-   void sendOdomData(
-     const geometry_msgs::msg::PoseStamped & pose,
-     const geometry_msgs::msg::Twist & velocity = geometry_msgs::msg::Twist());
+    void sendData(
+      const geometry_msgs::msg::PoseStamped & pose,
+      const geometry_msgs::msg::Twist & velocity, //= geometry_msgs::msg::Twist(),
+      const geometry_msgs::msg::PoseStamped & goal_pose);
 
-    void sendScanData(
-      const sensor_msgs::msg::LaserScan & scan);
- 
-   rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
-   std::shared_ptr<tf2_ros::Buffer> tf_;
-   std::string plugin_name_;
-   std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
-   rclcpp::Logger logger_ {rclcpp::get_logger("DRL_VO_Controller")};
-   rclcpp::Clock::SharedPtr clock_;
- 
-   rclcpp::Duration transform_tolerance_ {0, 0};
- 
-   nav_msgs::msg::Path global_plan_;
-   std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
-   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
- };
- 
- }  // namespace nav2py_template_controller
- 
- #endif  // NAV2PY_TEMPLATE_CONTROLLER__TEMPLATE_CONTROLLER_HPP_
+    nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped & pose);
+
+    bool transformPose(
+      const std::shared_ptr<tf2_ros::Buffer> tf,
+      const std::string frame,
+      const geometry_msgs::msg::PoseStamped & in_pose,
+      geometry_msgs::msg::PoseStamped & out_pose,
+      const rclcpp::Duration & transform_tolerance
+    ) const;
+
+    // scan subscriber callback:
+    void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+    void scanHistoryCallback(const std_msgs::msg::Float32MultiArray::SharedPtr msg);
+    // Timer:
+    void timerCallback();
+    
+    // pure pursit:
+    geometry_msgs::msg::Point circleSegmentIntersection(
+    const geometry_msgs::msg::Point & p1,
+    const geometry_msgs::msg::Point & p2,
+    double r);
+
+    geometry_msgs::msg::PoseStamped getLookAheadPoint(
+    const double & lookahead_dist,
+    const nav_msgs::msg::Path & transformed_plan,
+    bool interpolate_after_goal);
+  
+    rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+    std::shared_ptr<tf2_ros::Buffer> tf_;
+    std::string plugin_name_;
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+    rclcpp::Logger logger_ {rclcpp::get_logger("DrlVoController")};
+    rclcpp::Clock::SharedPtr clock_;
+
+
+    double lookahead_dist_;
+  
+    rclcpp::Duration transform_tolerance_ {0, 0};
+
+    // global plan pub:
+    nav_msgs::msg::Path global_plan_;
+    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
+
+    // scan sub:
+    sensor_msgs::msg::LaserScan scan_msg;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+
+    // scan history pub & sub:
+    std_msgs::msg::Float32MultiArray scan_history_msg;
+    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Float32MultiArray>> scan_history_pub_;
+    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr scan_history_sub_;
+
+    // Timer object:
+    rclcpp::TimerBase::SharedPtr timer_;
+    std::vector<float> scan_data_;
+    std::vector<std::vector<float>> scan_history_;
+    
+    int ts_cnt = 0;
+
+  };
+
+}  // namespace nav2py_drl_vo_controller
+
+#endif  // NAV2PY_TEMPLATE_CONTROLLER__TEMPLATE_CONTROLLER_HPP_
